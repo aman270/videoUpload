081efb54a20000048da6a7f7cbe276ebd87c948b
psremanache <psremanache6@gmail.com>
2020-09-04 11:24:05 +0530

    adding file

---
+++ uploadVideo/uploadVideo/Controllers/VideoOperation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using uploadVideo.Models;
+
+
+namespace uploadVideo.Controllers
+{
+    public class VideoOperation : Controller
+    {
+       
+        IGridFSBucket gridFS;   //file storage
+        IMongoCollection<Video> video; // collection in database
+
+        public VideoOperation()
+        {
+            //connection string
+            string connectionString = "mongodb://localhost:27017/VideoDB";
+            var connection = new MongoUrlBuilder(connectionString);
+            // get a client to interact with the database
+            MongoClient client = new MongoClient(connectionString);
+            // we get access to the database itself
+            IMongoDatabase database = client.GetDatabase(connection.DatabaseName);
+            // get access to file storage
+            gridFS = new GridFSBucket(database);
+            video = database.GetCollection<Video>("Videos");
+        }
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload( Video p, IFormFile uploadedFile)
+        {
+            if (ModelState.IsValid)
+            {
+                await video.InsertOneAsync(p);
+                ObjectId fileId = new ObjectId();
+                fileId= await gridFS.UploadFromStreamAsync(uploadedFile.FileName, uploadedFile.OpenReadStream());
+                p.fileId = fileId.ToString();
+                return RedirectToAction("Index","HomeController");
+            }
+            return View(p);
+        }
+    }
+}
---
+++ uploadVideo/uploadVideo/Models/Video.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace uploadVideo.Models
+{
+    public class Video
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [Display(Name = "Video Name")]
+        public string videoName { get; set; }
+
+        [Display(Name = "Type of Video")]
+        public string ContentType { get; set; }
+
+        public IFormFile file { set; get; }
+
+        public string fileId { get; set; }
+
+        public bool HasImage()
+        {
+            return !String.IsNullOrWhiteSpace(fileId);
+        }
+
+
+    }
+}
